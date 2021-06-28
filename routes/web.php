9e2232d980aec98eab143149475a7b53e594f719
mano@setjapan.co.jp <mano@setjapan.co.jp>
2021-06-28 15:36:34 +0900

    品番一覧画面の作成

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,17 @@ Route::group(['middleware' => 'web'], function () {
     Route::redirect('/', 'users')->name('home');
     
     Route::group(['middleware' => 'auth'], function () {
+        Route::prefix('items')
+            ->name('items.')
+            ->group(function () {
+                Route::get('/', 'ItemController@index')->name('list');                 // 一覧画面表示
+                Route::get('/create', 'ItemController@create')->name('create');        // 作成画面表示
+                // Route::post('/', 'ItemController@store')->name('store');               // 作成
+                Route::get('/{item_id}/edit', 'ItemController@edit')->name('edit');    // 更新画面表示
+                // Route::patch('/{item_id}', 'ItemController@update')->name('update');    // 更新
+                // Route::delete('/{item_id}', 'ItemController@destroy')->name('destroy');// 削除
+        });
+
         Route::prefix('users')
             ->name('users.')
             ->group(function () {
